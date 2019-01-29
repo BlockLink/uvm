e5c971db5a5c1ead746daa4a1e14193c7291f85c
qinzhang <azaaza321@163.com>
2019-01-29 10:43:17 +0800

    libjson2

--- src/uvm/linit.cpp
+++ src/uvm/linit.cpp
@@ -47,7 +47,8 @@ static const luaL_Reg loadedlibs[] = {
     { LUA_TIMELIBNAME, luaopen_time },
     { LUA_MATHLIBNAME, luaopen_math },
 	{ LUA_SAFEMATHLIBNAME, luaopen_safemath },
-    { LUA_JSONLIBNAME, luaopen_json },
+    //{ LUA_JSONLIBNAME, luaopen_json },
+	{ LUA_JSONLIBNAME, luaopen_json2 },
     { LUA_UTF8LIBNAME, luaopen_utf8 },
     { nullptr, nullptr }
 };
