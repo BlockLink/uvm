904d140a41c0e23de6ee74b14df2dd88e183d109
test blocklinker <test@blocklinker.com>
2019-03-18 17:42:22 +0800

    fix a typo errot

--- build_prepare.sh
+++ build_prepare.sh
@@ -2,6 +2,8 @@
 docker-compose build 
 container_id="uvm_uvmbuild_dev"
 project_dir=`pwd`
-image="uvmbuild_uvm"
-docker run -t -d -v $project_dir:/code --name $container_name $image /bin/bash
+image="uvmbuild_uvm:latest"
+echo $project_dir
+echo $image
+docker run -t -d -v $project_dir:/code --name $container_id $image /bin/bash
 
