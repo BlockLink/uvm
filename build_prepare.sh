8c2d5404370ac9c8670906dadd02499af8fd26db
zoowii <1992zhouwei@gmail.com>
2019-03-19 13:21:05 +0800

    change img name for dev

--- build_prepare.sh
+++ build_prepare.sh
@@ -2,7 +2,7 @@
 docker-compose build 
 container_id="uvm_uvmbuild_dev"
 project_dir=`pwd`
-image="uvmbuild_uvm:latest"
+image="blocklinkdev/uvmbuild_uvm:latest"
 echo $project_dir
 echo $image
 docker run -t -d -v $project_dir:/code --name $container_id $image /bin/bash
