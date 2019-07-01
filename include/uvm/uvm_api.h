5d059778cbc0902be17b47102923621eda24e558
zoowii <1992zhouwei@gmail.com>
2019-07-01 16:55:06 +0800

    add contract lockbalance and getpayback apis

--- include/uvm/uvm_api.h
+++ include/uvm/uvm_api.h
@@ -564,6 +564,33 @@ namespace uvm {
             virtual lua_Integer transfer_from_contract_to_address(lua_State *L, const char *contract_address, const char *to_address,
               const char *asset_type, int64_t amount) = 0;
 
+			/**
+			 * lockbalance and payback
+			 */
+			virtual bool lock_contract_balance_to_miner(lua_State *L, const char* cid, const char* asset_sym, const char* amount, const char* mid) {
+				throw_exception(L, UVM_API_SIMPLE_ERROR, "no implemented method");
+				return false;
+			}
+			virtual bool obtain_pay_back_balance(lua_State *L, const char* contract_addr, const char* mid, const char* sym_to_obtain, const char* amount) {
+				throw_exception(L, UVM_API_SIMPLE_ERROR, "no implemented method");
+				return false; 
+			}
+			virtual bool foreclose_balance_from_miners(lua_State *L, const char* foreclose_account, const char* mid, const char* sym_to_foreclose, const const char* amount) {
+				throw_exception(L, UVM_API_SIMPLE_ERROR, "no implemented method");
+				return false;
+			}
+			virtual std::string get_contact_lock_balance_info(lua_State *L, const char* mid) {
+				throw_exception(L, UVM_API_SIMPLE_ERROR, "no implemented method");
+				return "";
+			}
+			virtual std::string get_contact_lock_balance_info(lua_State *L, const char* cid, const char* aid)const {
+				return "";
+			}
+			virtual std::string get_pay_back_balacne(lua_State *L, const char* contract_addr, const char* symbol_type) {
+				throw_exception(L, UVM_API_SIMPLE_ERROR, "no implemented method");
+				return "";
+			}
+
             /************************************************************************/
             /* transfer asset from contract by account name on chain                */
             /************************************************************************/
