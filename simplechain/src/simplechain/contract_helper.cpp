1aded8023bc22f7484c10e5fa7063bf3e9c20552
qinzhang <azaaza321@163.com>
2020-03-03 11:11:24 +0800

    fix read api arg types

--- simplechain/src/simplechain/contract_helper.cpp
+++ simplechain/src/simplechain/contract_helper.cpp
@@ -323,12 +323,13 @@ free(storage_buf); \
 		unsigned int check_digest[5];
 		sha.process_bytes(code.code.data(), code.code.size());
 		sha.get_digest(check_digest);
+		
 		if (memcmp((void*)digest, (void*)check_digest, sizeof(unsigned int) * 5))
 		{
 			f->common_close();
 			throw uvm::core::UvmException("Verify bytescode SHA1 fail!");
 		}
-
+		
 		for (int i = 0; i < 5; ++i)
 		{
 			unsigned char chr1 = (check_digest[i] & 0xFF000000) >> 24;
@@ -370,15 +371,16 @@ free(storage_buf); \
 					f->common_close();
 					throw uvm::core::UvmException("Read verify code fail!");
 				}
+				if (api_name_len >= 128) {
+					f->common_close();
+					throw uvm::core::UvmException("api name str len must less than 128");
+				}
+
 				read_count = common_fread_octets(f, api_name_buf, api_name_len);
 				if (read_count != 1)
 				{
 					f->common_close();
 					throw uvm::core::UvmException("Read verify code fail!");
-				}
-				if (api_name_len >= 128) {
-					f->common_close();
-					throw uvm::core::UvmException("api name str len must less than 128");
 				}
 				api_name_buf[api_name_len] = '\0';
 				read_count = common_fread_int(f, (int*)&args_count);
@@ -403,60 +405,6 @@ free(storage_buf); \
 					types_vector.push_back(arg_type);
 				}
 				code.api_arg_types.insert(std::make_pair(std::string(api_name_buf), types_vector));
-			}
-		}
-
-		// read api args
-		{
-			int args_apis_count = 0;
-			auto read_count = common_fread_int(f, &args_apis_count);
-			if (read_count == 1) {
-				for (int i = 0; i < args_apis_count; i++)
-				{
-					int api_len = 0;
-					read_count = common_fread_int(f, &api_len);
-					if (read_count != 1)
-					{
-						f->common_close();
-						throw uvm::core::UvmException("Read verify code fail!");
-					}
-					api_buf = (char*)malloc(api_len + 1);
-					if (api_buf == NULL)
-					{
-						f->common_close();
-						FC_ASSERT(api_buf == NULL, "malloc fail!");
-					}
-					read_count = common_fread_octets(f, api_buf, api_len);
-					if (read_count != 1)
-					{
-						f->common_close();
-						free(api_buf);
-						throw uvm::core::UvmException("Read verify code fail!");
-					}
-					api_buf[api_len] = '\0';
-					std::string api_name(api_buf);
-					free(api_buf);
-					int args_count = 0;
-					read_count = common_fread_int(f, &args_count);
-					if (read_count != 1)
-					{
-						f->common_close();
-						free(api_buf);
-						throw uvm::core::UvmException("Read verify code fail!");
-					}
-					std::vector<UvmTypeInfoEnum> api_args;
-					for (int j = 0; j < args_count; j++) {
-						int arg_type = 0;
-						read_count = common_fread_int(f, (int*)&arg_type);
-						if (read_count != 1)
-						{
-							f->common_close();
-							throw uvm::core::UvmException("Read verify code fail!");
-						}
-						api_args.push_back(static_cast<UvmTypeInfoEnum>(arg_type));
-					}
-					code.api_arg_types[api_name] = api_args;
-				}
 			}
 		}
 
